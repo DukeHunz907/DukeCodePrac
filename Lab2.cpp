5aa28d71dbeb60b8b579e5715b19b55ad1212f82
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-09-22 04:52:42 +0000

    added

---
+++ Lab2.cpp
@@ -0,0 +1,15 @@
+//Write a C++ program that calculates the sum of all even numbers from 1 to 50 using a `while` loop
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int x;
+    
+
+
+
+
+
+
+}
