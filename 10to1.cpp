9dedcb2668855e1a7295ce1e62fd5ca06be2030d
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-09-22 04:45:01 +0000

    add 123

---
+++ 10to1.cpp
@@ -0,0 +1,17 @@
+// write a program that prints 1 to 10
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int i = 10;
+    while (i >= 1) {
+        cout << i << "\n";
+          i = i - 1;
+   
+    }
+   
+
+
+}
