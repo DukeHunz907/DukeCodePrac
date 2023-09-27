d26eeb99dc321dda89f73278bfa2c2943b61c80d
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-09-27 02:59:41 +0000

    add99

---
+++ Lab99.cpp
@@ -0,0 +1,15 @@
+//Develop a C++ program that uses a `while` loop to calculate and print the sum of the squares of the first 15 positive integers (1^2 + 2^2 + 3^2 + ... + 15^2).
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int 
+
+
+
+
+
+
+
+}
