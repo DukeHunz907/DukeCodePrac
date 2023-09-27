cd0d40cbce71352dc31f1242c8ade8c4f6df5557
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-09-27 04:18:47 +0000

    26

---
+++ Lab4.cpp
@@ -0,0 +1,22 @@
+//Write a C++ program that receives number items in an invoice from user (n) and-
+//-then receives price of those n items from user and prints total of prices of all items.
+
+#include <iostream>
+using namespace std;
+int main () {
+   cout << "number of items/n";
+   cin >> item;
+   cout << "price of items/n"
+    
+    while () {
+
+
+
+        
+    }
+
+}
+
+
+
+
