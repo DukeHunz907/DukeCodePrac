58829a05a681471eccc6f28286114b45bc3ffd72
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-09-29 04:46:04 +0000

    28

--- Lab8.cpp
+++ Lab8.cpp
@@ -1,2 +1,16 @@
 //Create a C++ program that uses a `while` loop to repeatedly ask the user for a positive integer until they enter a negative number.
 // Keep track of how many positive integers were entered and display that count at the end.
+
+#inculde <iostream>
+using namespace std;
+int main () {
+    int x = 0;
+    int y = 1;
+    while (y >= 0) {
+    
+    }
+
+
+
+
+}
