d6365cc0c2d9201f22ddec7b9f7f3e77dc25fd15
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-09-29 04:33:53 +0000

    28

---
+++ Lab7.cpp
@@ -0,0 +1,17 @@
+//Create a C++ program that uses a `while` loop to display the squares of numbers from 1 to 10.
+
+#include <iostream>
+using namespace std;
+int main () {
+    int n;
+    int i = 1;
+    while (i <= 10) {
+    n = i * i;
+  cout << n << "\n";
+   
+    i++;
+
+
+    }
+
+}
