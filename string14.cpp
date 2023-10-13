f9cb714b0ebbc7130812a091eed674c66c9f447a
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-10-13 03:48:32 +0000

    added attempts

---
+++ string14.cpp
@@ -0,0 +1,16 @@
+//First Rank: Create a program that receives a sentence from the user including a set of first names separated by space and prints out the name that
+//would show up first in alphabetical order. You can assume no two names in the list share the same first character. Example: if the input is "John Lauren
+//Samuel Nicholas Eva", the output should be "Eva".
+#include <iostream>
+using namespace std;
+int main () {
+    string names;
+    getline(cin, name);
+    string nextName = "";
+    string firstRankName = "";
+    int i = 0;
+    while (i < names.legnth()) {
+        
+    }
+
+}
---
+++ stringlab2.cpp
@@ -0,0 +1,13 @@
+//Swap Words: Write a program that reverses the order of the two words in given text string that has two words separated by a space character. 
+//For example, if the input is "hello world" the program should output "world hello".
+
+#include <iostream>
+using namespace std;
+int main () {
+    string s1, s2;
+    getling(cin, s1);
+    getline(cin, s2);
+    int i = 0;
+    cout << "Enter a text";
+    while (i < s1.legnth)
+}
