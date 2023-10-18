79548a14f665edb8024d75882b302e5b4a4bb670
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-10-18 04:37:51 +0000

    added10/17

---
+++ stringlab3.cpp
@@ -0,0 +1,14 @@
+//Longest Word: Write a program that receive a sentence and prints out length of the longest word in the string.
+//The text has no punctuation and only words each separated by one space.
+//Can you change the program to print out the longest word itself too? 
+#include <iostream>
+using namespace std;
+int main () {
+    string s, w;
+    int maxLen =0;
+    int i = 0;
+    getline(cin, s);
+    while (i < s.length()) {
+
+    }
+}
---
+++ stringlab4.cpp
@@ -0,0 +1,3 @@
+//Create a program that receives a sentence from the user including a set of first names separated by space and prints out the name that
+//would show up first in alphabetical order. You cannot assume no two names in the list share the same first character. Example: if the input is "John
+//Lauren Ashton Nicholas Ava", the output should be "Ashton". 
