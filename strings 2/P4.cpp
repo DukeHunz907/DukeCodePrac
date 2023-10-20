b0957f41c8eae93b7c89553245816ccd27439a84
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-10-20 03:16:57 +0000

    dd

---
+++ strings 2/P4.cpp
@@ -0,0 +1,8 @@
+//Multiplication Table: Create a C++ program that uses nested loops to print a multiplication table from 1 to 10, similar to the following format:
+1x1=1     1x2=2 ...      1x10=10
+2x1=2     2x2=4 ...      2x10=20
+#include<iostream>
+using namespace std;
+int main () {
+    
+}
