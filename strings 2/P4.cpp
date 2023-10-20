daa6a407c5f7fc474116bb24e51b5906f2e3b2da
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-10-20 03:34:55 +0000

    added 10/19

--- strings 2/P4.cpp
+++ strings 2/P4.cpp
@@ -4,5 +4,21 @@
 #include<iostream>
 using namespace std;
 int main () {
+    int i = 1;
+    int j = 1;
+    while (j <= 10) {
+        i = 1;
+          while (i <= 10) {
+        cout << j << " X " << i << " = " << j * i << "  ";
+        i++;
+    }
+
+
+    }
+   
+    }
     
-}
+
+
+
+ i++
