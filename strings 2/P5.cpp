d2759bf99ae976df3b234fd6b771f037c545d48a
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-10-20 03:43:04 +0000

    added10/19

---
+++ strings 2/P5.cpp
@@ -0,0 +1,7 @@
+//Chessboard Pattern: Create a program to print a chessboard pattern of 'n' rows and 'm' columns, with alternating 'X' and 'O' characters. The user inputs 'n' and 'm'.
+
+#include<iostream>
+using namespace std;
+int main () {
+    
+}
