1d02d6d7491789292f7ff1af9964fb23c905c86b
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-10-25 03:00:51 +0000

    10/24

---
+++ strings 2/p8.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+using namespace std;
+int main () {
+    string inputStr;
+    getline(cin, inputStr);
+    int countA = 0; 
+    for (int i = 0; i < inputStr.length(); i++) {
+        switch
+    }
+
+} 
+
