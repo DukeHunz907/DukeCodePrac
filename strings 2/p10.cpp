9704a10ff686410a50f4a138d9b8a0b775c2ae54
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-10-25 03:30:27 +0000

    10/24

---
+++ strings 2/p10.cpp
@@ -0,0 +1,7 @@
+//Create a program that asks the user to enter a string and a character. Then, use a `for` loop to count and display the number of occurrences of that character in the string.
+
+#include <iostream>
+using namespace std;
+int main () {
+    
+}
---
+++ strings 2/p9.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main () {
+    //cout << (int) 'a' << " " << (int) 'A' << " " << (int) 'z' << " " << (int) 'Z' << endl;
+
+    string inputStr;
+    getline(cin, inputStr);
+    for (int i = 0; i < input.length(); i++) {
+        if (inputStr[i] >= 97 and inputStr[i] <= 122 and (i == 0 or inputStr[i-1] == ''))
+
+    }
+}
