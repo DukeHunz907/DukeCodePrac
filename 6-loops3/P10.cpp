53b360e1fede6fc82b5aa6e97136ce5e61c966c8
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-11-08 04:23:38 +0000

    11/7

---
+++ 6-loops3/P10.cpp
@@ -0,0 +1,12 @@
+//Write a C++ program that receives an integer from the user and calculates the sum of its digits.
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Enter a number: ";
+    int x, sum = 0;
+    cin >> x, sum;
+    while()
+
+
+}
---
+++ 6-loops3/P11.cpp
@@ -0,0 +1,12 @@
+// Develop a C++ program that asks the user to enter a string and then uses a `for` loop to determine if it's a palindrome (reads the same forwards and backwards). 
+//Run the program in a way that number of loop's iteration is approximately equal to half of length of the string. 
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Enter a string: ";
+    string s1;
+     cin >> s1;
+    for()
+
+}
