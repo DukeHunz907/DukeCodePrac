abf01fbae51e37fb2f0640431916f158ae96cce3
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-11-08 01:46:53 +0000

    11/7

---
+++ 3-loops/P1.cpp
@@ -0,0 +1,10 @@
+//Write a program that uses a while to print numbers from 10 to 1. 
+#include <iostream>
+using namespace std;
+int main() {
+    int c = 10;
+    while(c >= 1) {
+        cout << c << endl;
+        c--;
+    }
+}
