eb2bd38e1b64491da2a4ca78bcd33e49d43dc75a
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-11-10 05:48:10 +0000

    added 11/9

--- 6-loops3/P15.cpp
+++ 6-loops3/P15.cpp
@@ -7,5 +7,7 @@ int main() {
     string s1, s2;
     getline(cin, s1);
     getline(cin, s2);
-    
+    for(int i = 0; i < s1.length() - 1; i += 2){
+        
+    }
 }
---
+++ 6-loops3/P16.cpp
@@ -0,0 +1,9 @@
+//Write a C++ program that receives a string of digits as input. The program should keep each digit only if it is smaller than all the subsequent digits in the string. 
+//All other digits should be removed. The order of the remaining digits should be preserved.
+//Example: Input: "547839216" Output: "16"
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Enter a string of numbers: " << endl;
+    
+}
