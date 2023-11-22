3eb6c90249b54a6661a7a877931c45aa1ae0f8e3
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-11-22 04:24:57 +0000

    added 11/21

---
+++ 7-vector/P5.cpp
@@ -0,0 +1,3 @@
+//Write a program to receive integers (stop when 0 is entered) from the input, put them in a vector and then prints out the second largest number of the vector. 
+//Example input: 12, 2, 5, 9, 13, 3, 23, 9, 0
+//Output: 13
---
+++ 7-vector/P6.cpp
@@ -0,0 +1,9 @@
+//Write a program to receive integers (stop when 0 entered), put them in a vector and then prints out sum of all numbers except maximum value in the list.
+//Example input: 12, 2, 5, 9, 13, 3, 23, 9, 0
+//Output: 53
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+
+}
