e8dbdb3c2b911b97e0c3775996d6f1cde2d07ee7
DukeHunz907 <144873867+DukeHunz907@users.noreply.github.com>
2023-12-13 03:57:25 +0000

    added 12/12

---
+++ 10-mini-proj/P4.cpp
@@ -0,0 +1,11 @@
+//Search Element in Array from File: Write a C++ program that defines a function `searchElement` to check if a given integer exists in an array. 
+//Read 20 elements of the array from a text file named "input_search.txt" and utilize the `searchElement` function to determine whether a number exists 
+//in the array or not, and display the result. Read the number to search from the input and repeat the process of searching elements until the user enters -1.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+    
+}
+
